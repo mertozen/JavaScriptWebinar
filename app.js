a396efedbe5265ffd7af20fc6ef9c99a7edc512d
mertozen <mert.ozen@gmail.com>
2016-11-21 00:40:26 +0200

    JavaScripWebinar

---
+++ app.js
@@ -0,0 +1,71 @@
+
+/*var Person = {
+
+name: 'Bora',
+Surname: 'Kasmer',
+Sex: 'Male',
+
+Parent: {
+Mother: 'Ozlem',
+Father: 'Erdinc',
+TCNO:343434343
+}
+
+}
+
+WriteName();
+console.log(say);
+function WriteName() {
+    console.log( Person.name + ' ' + Person.Surname);
+}
+
+var say = 'Wellcome To Webinar';
+*/
+
+/* function b() {
+
+    console.log(myVar);
+}
+
+function a() {
+    
+    console.log(myVar);
+    var myVar;
+    b();
+}
+var myVar = 1;
+a();
+console.log(myVar);
+*/
+
+/*function a() {
+    function b()
+    {
+        console.log(myVar);
+    
+    }
+    myVar = 2;
+    b();
+}
+
+var myVar = 1;
+a();
+b();
+*/
+
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms) {}
+    console.log("Action Finished");
+        
+}
+
+function clickHandler() {
+    console.log("click event");
+}
+
+document.addEventListener('click',clickHandler);
+waitThreeSeconds();
+console.log("Finish Execution");
+
+
