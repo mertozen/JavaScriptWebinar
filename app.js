a0b71eabf7b12a99b7eb6ea9863e2bceef2d325b
mertozen <mert.ozen@gmail.com>
2016-11-23 10:45:21 +0200

    commit 4

--- app.js
+++ app.js
@@ -84,4 +84,65 @@ Hello(0);
 */
 
 
+/*
+var callFunction = function(a){
+
+    console.log(a);
+}
+
+callFunction.Name = 'Mert Özen';
+callFunction(5);
+console.log(callFunction.Name);
+*/
+/*
+var callFunction = function (a)
+{
+  a();
+
+}
+
+callFunction(function() {
+
+var webinar = {
+
+    Unit:'JavaScript',
+    Speaker:'Mert Özen'
+}
+console.log(webinar.Speaker);
+console.log(webinar.Unit);
+});
+
+*/
+
+var  b = function (){
+
+console.log(this);
+
+}
+
+var c = {
+name:'First Name',
+log: function (){
+this.name  = 'C object name Updated';
+console.log(this);
+var updateIsim = function (_name){
+console.log(this);
+this.name = _name;
+
+}
+
+updateIsim('Bora');
+console.log(this);
+
+}
+}
+
+b();
+c.log();
+console.log(this);
+
+
+
+
+
 
